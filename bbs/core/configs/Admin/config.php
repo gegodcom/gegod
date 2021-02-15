fd43271534783c8b97f955ec8cd540ea2a9c21c9
gegod <79079922+gegodcom@users.noreply.github.com>
2021-02-15 20:45:35 +0800

    Add files via upload

---
+++ bbs/core/configs/Admin/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * TXTCMS ÏîÄ¿ÅäÖÃ
+ * @copyright			(C) 2013-2014 TXTCMS
+ * @license				http://www.txtcms.com
+ * @lastmodify			2014-8-8
+ */
+defined('INI_TXTCMS') or exit();
+return array(
+	'URL_MODEL'=>2,
+	'URL_PATH_DEPR'=>'-',
+	'TMPL_COMPILE_CHECK'=>true,
+	'TMPL_HTML_CACHE'=>false,
+	'DEFAULT_THEME'=>'',
+	'cms_version'=>'x1.1',
+	'cms_release'=>'20140818',
+);
